9af477e67af601682e508427bf35a2d534fcfff4
Pedro Hnerique <ph8464803@gmail.com>
2022-04-03 03:15:35 -0300

    refactor: 🚧
    service

--- lib/app/app_controller.dart
+++ lib/app/app_controller.dart
@@ -1,16 +1,25 @@
+import 'package:consume_api/app/interfaces/local_storage_iterface.dart';
 import 'package:consume_api/app/models/appconfig_model.dart';
-import 'package:flutter/src/foundation/change_notifier.dart' show ValueNotifier;
+import 'package:consume_api/app/services/shared_local_storage_service.dart';
+import 'package:flutter/src/foundation/change_notifier.dart';
 
 class AppController {
   static final AppController instance = AppController._();
 
-  AppController._();
+  AppController._() {
+    storage.get('isDark').then((value) {
+      config.themeSwitch.value = value;
+    });
+  }
 
   final AppConfigModel config = AppConfigModel();
   bool get isDark => config.themeSwitch.value;
   ValueNotifier<bool> get themeSwitch => config.themeSwitch;
 
+  final ILocalStorage storage = SharedLocalStorage();
+
   changeTheme(bool value) {
     config.themeSwitch.value = value;
+    storage.put('isDark', value);
   }
 }
---
+++ lib/app/services/shared_local_storage_service.dart
@@ -0,0 +1,30 @@
+import 'package:consume_api/app/interfaces/local_storage_iterface.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class SharedLocalStorage implements ILocalStorage {
+  @override
+  Future delete(String key) async {
+    var shared = await SharedPreferences.getInstance();
+    return shared.remove(key);
+  }
+
+  @override
+  Future get(String key) async {
+    var shared = await SharedPreferences.getInstance();
+    return shared.get(key);
+  }
+
+  @override
+  Future put(String key, value) async {
+    var shared = await SharedPreferences.getInstance();
+    if (value is bool) {
+      shared.setBool(key, value);
+    } else if (value is String) {
+      shared.setString(key, value);
+    } else if (value is int) {
+      shared.setInt(key, value);
+    } else if (value is double) {
+      shared.setDouble(key, value);
+    }
+  }
+}
